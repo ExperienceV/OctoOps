e577f755a3869ca7e6b24dc10ad7b8524be381c0
ExperienceV <132963842+ExperienceV@users.noreply.github.com>
2026-08-19 21:50:55 -0500

    feat: Implement collector of metrics

---
+++ agent/internal/collector/collector.go
@@ -0,0 +1,48 @@
+package collector
+
+import "github.com/ExperienceV/OctoOps/agent/contracts"
+
+type Collector struct{}
+
+func New() Collector {
+	return Collector{}
+}
+
+func (Collector) Collect() (contracts.Metrics, error) {
+	cpuPercent, err := CPUPercent()
+	if err != nil {
+		return contracts.Metrics{}, err
+	}
+
+	cpuTemp, err := CPUTemp()
+	if err != nil {
+		return contracts.Metrics{}, err
+	}
+
+	memoryTotal, err := MemoryTotal()
+	if err != nil {
+		return contracts.Metrics{}, err
+	}
+
+	memoryUsed, err := MemoryUsed()
+	if err != nil {
+		return contracts.Metrics{}, err
+	}
+
+	memoryFree, err := MemoryFree()
+	if err != nil {
+		return contracts.Metrics{}, err
+	}
+
+	return contracts.Metrics{
+		CPU: contracts.CpuMetrics{
+			Percent: cpuPercent,
+			Temp:    cpuTemp,
+		},
+		Memory: contracts.MemMetrics{
+			Total: memoryTotal,
+			Used:  memoryUsed,
+			Free:  memoryFree,
+		},
+	}, nil
+}
